4dfba4dc8f4441bf355ffa1ca1577c6496a603df
Alexey Sverchkov <alexey.sverchkov@mail.ru>
2019-05-05 11:36:17 +0300

    added file with rotations

---
+++ Red_Black_Tree/Red_Black_Tree/Rotations.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Node.h"
+
+template<typename T>
+void LeftLeftRotation(Node<T>* addedNode);
+
+template<typename T>
+void LeftRightRotation(Node<T>* addedNode);
+
+template<typename T>
+void RightRightRotation(Node<T>* addedNode);
+
+template<typename T>
+void RightLeftRotation(Node<T>* addedNode);
