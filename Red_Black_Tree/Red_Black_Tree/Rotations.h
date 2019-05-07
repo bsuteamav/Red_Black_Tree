ef9fa192d060d5a2228a11170a8706450772ed63
Alexey <alexey.sverchkov@mail.ru>
2019-05-07 12:32:26 +0300

    mb fix bugs in LL and LR rotations

--- Red_Black_Tree/Red_Black_Tree/Rotations.h
+++ Red_Black_Tree/Red_Black_Tree/Rotations.h
@@ -2,42 +2,51 @@
 #include "Node.h"
 
 template<typename T>
-void LeftLeftRotation(Node<T>*& addedNode);
+void LeftLeftRotation(Node<T>* addedNode);
 
 template<typename T>
-void LeftRightRotation(Node<T>*& addedNode);
+void LeftRightRotation(Node<T>* addedNode);
 
 template<typename T>
-void RightRightRotation(Node<T>*& addedNode);
+void RightRightRotation(Node<T>* addedNode);
 
 template<typename T>
-void RightLeftRotation(Node<T>*& addedNode);
+void RightLeftRotation(Node<T>* addedNode);
 
 
 
 template<typename T>
-inline void LeftLeftRotation(Node<T>*& addedNode)
+inline void LeftLeftRotation(Node<T>* addedNode)
 {
 	/* right son of parent becomes left son of grandparent;
 	   grandparent becomes parent for right son of parent
 	*/
 	addedNode->parent->parent->left = addedNode->parent->right;
-	addedNode->parent->right->parent = addedNode->parent->parent;
+	if (addedNode->parent->right != nullptr) {
+		addedNode->parent->right->parent = addedNode->parent->parent;
+	}
 	
 	// grandparent becomes right son of parent
 	addedNode->parent->right = addedNode->parent->parent;
 
-	// GP's left son is G
-	if (addedNode->parent->parent->parent->left == addedNode->parent->parent) {
-		// P's parent = GP; GP's left son = P
-		addedNode->parent->parent = addedNode->parent->parent->parent;
-		addedNode->parent->parent->parent->left = addedNode->parent;
+	// GP is not nullptr
+	if (addedNode->parent->parent != nullptr) {
+		// GP's left son is G
+		if (addedNode->parent->parent->parent->left == addedNode->parent->parent) {
+			// P's parent = GP; GP's left son = P
+			addedNode->parent->parent = addedNode->parent->parent->parent;
+			addedNode->parent->parent->parent->left = addedNode->parent;
+		}
+		// GP's right son is G
+		else {
+			// P's parent = GP; GP's right son = P
+			addedNode->parent->parent = addedNode->parent->parent->parent;
+			addedNode->parent->parent->parent->right = addedNode->parent;
+		}
 	}
-	// GP's right son is G
+	// GP is nullptr
 	else {
-		// P's parent = GP; GP's right son = P
-		addedNode->parent->parent = addedNode->parent->parent->parent;
-		addedNode->parent->parent->parent->right = addedNode->parent;
+		addedNode->parent->parent = nullptr;
 	}
 
 	//  G's parent = P
@@ -49,13 +58,16 @@ inline void LeftLeftRotation(Node<T>*& addedNode)
 }
 
 template<typename T>
-inline void LeftRightRotation(Node<T>*& addedNode)
+inline void LeftRightRotation(Node<T>* addedNode)
 {
 	/* P's right son = X's left son
 	   X's left son's parent = P
 	*/
 	addedNode->parent->right = addedNode->left;
-	addedNode->left->parent = addedNode->parent;
+	// X's left son != nullptr
+	if (addedNode->left != nullptr) {
+		addedNode->left->parent = addedNode->parent;
+	}
 
 	/* X's left son = P
 	   X's parent = G
