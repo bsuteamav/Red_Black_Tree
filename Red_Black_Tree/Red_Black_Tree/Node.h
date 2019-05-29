a966543c79317710ae5117795af6b52e5c646a21
Alexey <alexey.sverchkov@mail.ru>
2019-05-29 23:17:35 +0300

    added methods sibling and uncle for node

--- Red_Black_Tree/Red_Black_Tree/Node.h
+++ Red_Black_Tree/Red_Black_Tree/Node.h
@@ -13,4 +13,38 @@ struct Node {
 	int height;
 	int	depth;
 	bool isDoubleBlack = false;
+
+	// check if node is left child of parent
+	bool isOnLeft() { return this == parent->left; }
+
+	// returns pointer to uncle
+	Node<T>* uncle() {
+		// if no parent or grandparent, then no uncle
+		if (parent == nullptr || parent->parent == nullptr) {
+			return nullptr;
+		}
+		if (parent->isOnLeft()) {
+			// uncle is on right
+			return parent->parent->right;
+		}
+		else {
+			// uncle is on left
+			return parent->parent->left;
+		}
+	}
+
+	// returns pointer to sibling
+	Node<T>* sibling() {
+		// sibling null if no parent
+		if (parent == nullptr) {
+			return nullptr;
+		}
+		// this node is left son -> sibling is right son of parent
+		if (isOnLeft()) {
+			return parent->right;
+		}
+		else {
+			return parent->left;
+		}
+	}
 };
