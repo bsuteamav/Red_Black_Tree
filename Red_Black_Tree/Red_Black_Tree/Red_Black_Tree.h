5d6a2cdf8fa90ce6e1605b24eef79d234d1bad55
Alexey <alexey.sverchkov@mail.ru>
2019-05-30 19:10:33 +0300

    added working deleting method

--- Red_Black_Tree/Red_Black_Tree/Red_Black_Tree.h
+++ Red_Black_Tree/Red_Black_Tree/Red_Black_Tree.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include "Node.h"
 #include "Calculate.h"
+#include "swap.h"
 
 using namespace std;
 
@@ -16,9 +17,18 @@ private:
 	Node<T>* search(T data, Node<T>* Tree);
     void balance(Node<T>*& Tree);
 	void cleanup(Node<T>* tree); // for destructor
+
+	void leftRotate(Node<T>* x);
+	void rightRotate(Node<T>* x);
+	Node<T>* predecessor(Node<T>* x);
+	Node<T>* BSTreplace(Node<T>* x);
+	void fixDoubleBlack(Node<T>* x);
+	void deleteNode(Node<T>* v);
+
 public:
     Red_Black_Tree();
     void addNode(T data);
+	void deleteNode(T data);
     void show();
     ~Red_Black_Tree();
 };
@@ -36,6 +46,23 @@ Red_Black_Tree<T>::~Red_Black_Tree() {
 template <typename T>
 void Red_Black_Tree<T>::addNode(T data) {
     this->addNode(data, root);
+}
+
+template<typename T>
+inline void Red_Black_Tree<T>::deleteNode(T data)
+{
+	if (root == nullptr) {
+		// tree is empty
+		return;
+	}
+	Node<T>* v = search(data, this->root);
+
+	if (v->key != data) {
+		cout << "There isn't node with this value:\n";
+		return;
+	}
+
+	deleteNode(v);
 }
 
 template <typename T>
@@ -191,6 +218,233 @@ inline void Red_Black_Tree<T>::cleanup(Node<T>* tree)
 		delete tree;
 	}
 }
+
+template<typename T>
+inline void Red_Black_Tree<T>::leftRotate(Node<T>* x)
+{
+	// new parent will be node's right child 
+	Node<T>* nParent = x->right;
+
+	// update root if current node is root 
+	if (x == root)
+		root = nParent;
+
+	x->moveDown(nParent);
+
+	// connect x with new parent's left element 
+	x->right = nParent->left;
+	// connect new parent's left element with node 
+	// if it is not null 
+	if (nParent->left != nullptr)
+		nParent->left->parent = x;
+
+	// connect new parent with x 
+	nParent->left = x;
+}
+
+template<typename T>
+inline void Red_Black_Tree<T>::rightRotate(Node<T>* x)
+{
+	// new parent will be node's left child 
+	Node<T>* nParent = x->left;
+
+	// update root if current node is root 
+	if (x == root)
+		root = nParent;
+
+	x->moveDown(nParent);
+
+	// connect x with new parent's right element 
+	x->left = nParent->right;
+	// connect new parent's right element with node 
+	// if it is not null 
+	if (nParent->right != nullptr)
+		nParent->right->parent = x;
+
+	// connect new parent with x 
+	nParent->right = x;
+}
+
+template<typename T>
+inline Node<T>* Red_Black_Tree<T>::predecessor(Node<T>* x)
+{
+	Node<T>* temp = x;
+	while (temp->right != nullptr) {
+		temp = temp->right;
+	}
+	return temp;
+}
+
+template<typename T>
+inline Node<T>* Red_Black_Tree<T>::BSTreplace(Node<T>* x)
+{
+	// when node have 2 children 
+	if (x->left != nullptr and x->right != nullptr)
+		return predecessor(x->left);
+
+	// when leaf 
+	if (x->left == nullptr and x->right == nullptr)
+		return nullptr;
+
+	// when single child 
+	if (x->left != nullptr)
+		return x->left;
+	else
+		return x->right;
+}
+
+template<typename T>
+inline void Red_Black_Tree<T>::fixDoubleBlack(Node<T>* x)
+{
+	if (x == root)
+		// Reached root 
+		return;
+
+	Node<T>* sibling = x->sibling();
+	Node<T>* parent = x->parent;
+	if (sibling == nullptr) {
+		// No sibiling, double black pushed up 
+		fixDoubleBlack(parent);
+	}
+	else {
+		if (sibling->color == RED) {
+			// Sibling red 
+			parent->color = RED;
+			sibling->color = BLACK;
+			if (sibling->isOnLeft()) {
+				// left case 
+				rightRotate(parent);
+			}
+			else {
+				// right case 
+				leftRotate(parent);
+			}
+			fixDoubleBlack(x);
+		}
+		else {
+			// Sibling black 
+			if (sibling->hasRedChild()) {
+				// at least 1 red children 
+				if (sibling->left != nullptr && sibling->left->color == RED) {
+					if (sibling->isOnLeft()) {
+						// left left 
+						sibling->left->color = sibling->color;
+						sibling->color = parent->color;
+						rightRotate(parent);
+					}
+					else {
+						// right left 
+						sibling->left->color = parent->color;
+						rightRotate(sibling);
+						leftRotate(parent);
+					}
+				}
+				else {
+					if (sibling->isOnLeft()) {
+						// left right 
+						sibling->right->color = parent->color;
+						leftRotate(sibling);
+						rightRotate(parent);
+					}
+					else {
+						// right right 
+						sibling->right->color = sibling->color;
+						sibling->color = parent->color;
+						leftRotate(parent);
+					}
+				}
+				parent->color = BLACK;
+			}
+			else {
+				// 2 black children 
+				sibling->color = RED;
+				if (parent->color == BLACK)
+					fixDoubleBlack(parent);
+				else
+					parent->color = BLACK;
+			}
+		}
+	}
+}
+
+template<typename T>
+inline void Red_Black_Tree<T>::deleteNode(Node<T>* v)
+{
+	Node<T>* u = BSTreplace(v);
+
+	// True when u and v are both black 
+	bool uvBlack = ((u == nullptr || u->color == BLACK) && (v->color == BLACK));
+	Node<T>* parent = v->parent;
+
+	if (u == nullptr) {
+		// u is nullptr therefore v is leaf 
+		if (v == root) {
+			// v is root, making root null 
+			root = nullptr;
+		}
+		else {
+			if (uvBlack) {
+				// u and v both black 
+				// v is leaf, fix double black at v 
+				fixDoubleBlack(v);
+			}
+			else {
+				// u or v is red 
+				if (v->sibling() != nullptr) {
+					// sibling is not null, make it red" 
+					v->sibling()->color = RED;
+				}
+			}
+
+			// delete v from the tree 
+			if (v->isOnLeft()) {
+				parent->left = nullptr;
+			}
+			else {
+				parent->right = nullptr;
+			}
+		}
+		delete v;
+		return;
+	}
+
+	if (v->left == nullptr or v->right == nullptr) {
+		// v has 1 child 
+		if (v == root) {
+			// v is root, assign the value of u to v, and delete u 
+			v->key = u->key;
+			v->left = v->right = nullptr;
+			delete u;
+		}
+		else {
+			// Detach v from tree and move u up 
+			if (v->isOnLeft()) {
+				parent->left = u;
+			}
+			else {
+				parent->right = u;
+			}
+			delete v;
+			u->parent = parent;
+			if (uvBlack) {
+				// u and v both black, fix double black at u 
+				fixDoubleBlack(u);
+			}
+			else {
+				// u or v red, color u black 
+				u->color = BLACK;
+			}
+		}
+		return;
+	}
+
+	// v has 2 children, swap values with successor and recurse 
+	swapValues(u, v);
+	deleteNode(u);
+
+}
+
+
 
 template <typename T>
 void Red_Black_Tree<T>::show()
